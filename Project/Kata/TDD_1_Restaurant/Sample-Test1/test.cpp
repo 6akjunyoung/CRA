2b0ec76c9a7d41c6d82ab4e73bee9a330f719c66
6akjunyoung <6akjunyoung@gmail.com>
2024-07-04 15:49:24 +0900

    Restaurant Booking: 1. TC refact. 및 보강

--- Project/Kata/TDD_1_Restaurant/Sample-Test1/test.cpp
+++ Project/Kata/TDD_1_Restaurant/Sample-Test1/test.cpp
@@ -1,20 +1,46 @@
 #include "pch.h"
 #include "../TDD_1_Restaurant/BookingScheduler.cpp"
 
-TEST(BookingSchedulerTest, t1) {//예약은_정시에만_가능하다_정시가_아닌경우_예약불가) {
-	//arrange
-	tm notOnTheHour = { 0 };
-	notOnTheHour.tm_year = 2021 - 1900;
-	notOnTheHour.tm_mon = 03 - 1;
-	notOnTheHour.tm_mday = 26;
-	notOnTheHour.tm_hour = 9;
-	notOnTheHour.tm_min = 5;
-	notOnTheHour.tm_isdst = -1;
-	mktime(&notOnTheHour);
+#include <iostream>
+#include <stdexcept>
 
-	Customer customer{ "Fake name", "010-1234-5678" };
-	Schedule* schedule = new Schedule{ notOnTheHour, 1, customer };
-	BookingScheduler bookingScheduler{ 3 };
+using namespace std;
+
+class BookingItem : public testing::Test {
+protected:
+	void SetUp() override
+	{
+		NOT_ON_THE_HOUR = getTime(2021, 3, 26, 9, 05);
+		ON_THE_HOUR = getTime(2021, 3, 26, 9, 00);
+	}
+
+public:
+	tm getTime(int year, int mon, int day, int hour, int min)
+	{
+		tm result = { 0, min, hour, day, mon - 1, year - 1900, 0, 0, -1 };
+		mktime(&result);
+		return result;
+	}
+
+	tm plusHour(tm base, int hour)
+	{
+		base.tm_hour += hour;
+		mktime(&base);
+		return base;
+	}
+
+	tm NOT_ON_THE_HOUR;
+	tm ON_THE_HOUR;
+	Customer CUSTOMER{ "Fake name", "010-1234-5678" };
+	const int UNDER_CAPACITY = 1;
+	const int CAPACITY_PER_HOUR = 3;
+
+	BookingScheduler bookingScheduler{ CAPACITY_PER_HOUR };
+};
+
+TEST_F(BookingItem, t1) {//예약은_정시에만_가능하다_정시가_아닌경우_예약불가) {
+	//arrange
+	Schedule* schedule = new Schedule{ NOT_ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
 
 	//act
 	EXPECT_THROW({
@@ -25,20 +51,9 @@ TEST(BookingSchedulerTest, t1) {//예약은_정시에만_가능하다_정시가_
 	//expected runtime exception
 }
 
-TEST(BookingSchedulerTest, t2) { //예약은_정시에만_가능하다_정시인_경우_예약가능) {
+TEST_F(BookingItem, t2) { //예약은_정시에만_가능하다_정시인_경우_예약가능) {
 	//arrange
-	tm onTheHour = { 0 };
-	onTheHour.tm_year = 2021 - 1900;
-	onTheHour.tm_mon = 03 - 1;
-	onTheHour.tm_mday = 26;
-	onTheHour.tm_hour = 9;
-	onTheHour.tm_min = 0;
-	onTheHour.tm_isdst = -1;
-	mktime(&onTheHour);
-
-	Customer customer{ "Fake name", "010-1234-5678" };
-	Schedule* schedule = new Schedule{ onTheHour, 1, customer };
-	BookingScheduler bookingScheduler{ 3 };
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
 
 	//act
 	bookingScheduler.addSchedule(schedule);
@@ -47,30 +62,58 @@ TEST(BookingSchedulerTest, t2) { //예약은_정시에만_가능하다_정시인
 	EXPECT_EQ(true, bookingScheduler.hasSchedule(schedule));
 }
 
-TEST(BookingSchedulerTest, t3) { //시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생) {
+TEST_F(BookingItem, t3) { //시간대별_인원제한이_있다_같은_시간대에_Capacity_초과할_경우_예외발생) {
+	//arrange
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, CAPACITY_PER_HOUR, CUSTOMER };
+	bookingScheduler.addSchedule(schedule);
+
+	try {
+		//act
+		Schedule* newSchedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
+		bookingScheduler.addSchedule(newSchedule);
+		FAIL();
+	}
+	catch (runtime_error& e) {
+		//assert
+		EXPECT_EQ(string{ "Number of people is over restaurant capacity per hour" }, string{ e.what() });
+	}
+	catch (...)
+	{
+		FAIL();
+	}
+}
+
+TEST_F(BookingItem, t4) { //시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공) {
+	//arrange
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
+	bookingScheduler.addSchedule(schedule);
+
+	//act
+	tm differentTime = plusHour(ON_THE_HOUR, 1);
+	Schedule* newSchedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
+	bookingScheduler.addSchedule(newSchedule);
+
+	//assert
+	EXPECT_EQ(true, bookingScheduler.hasSchedule(schedule));
+	EXPECT_EQ(true, bookingScheduler.hasSchedule(newSchedule));
+}
+
+TEST_F(BookingItem, t5) { //예약완료시_SMS는_무조건_발송) {
 
 }
 
-TEST(BookingSchedulerTest, t4) { //시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공) {
+TEST_F(BookingItem, t6) { //이메일이_없는_경우에는_이메일_미발송) {
 
 }
 
-TEST(BookingSchedulerTest, t5) { //예약완료시_SMS는_무조건_발송) {
+TEST_F(BookingItem, t7) { //이메일이_있는_경우에는_이메일_발송) {
 
 }
 
-TEST(BookingSchedulerTest, t6) { //이메일이_없는_경우에는_이메일_미발송) {
+TEST_F(BookingItem, t8) { //현재날짜가_일요일인_경우_예약불가_예외처리) {
 
 }
 
-TEST(BookingSchedulerTest, t7) { //이메일이_있는_경우에는_이메일_발송) {
-
-}
-
-TEST(BookingSchedulerTest, t8) { //현재날짜가_일요일인_경우_예약불가_예외처리) {
-
-}
-
-TEST(BookingSchedulerTest, t9) { //현재날짜가_일요일이_아닌경우_예약가능) {
+TEST_F(BookingItem, t9) { //현재날짜가_일요일이_아닌경우_예약가능) {
 
 }
