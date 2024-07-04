b688c6cd7068bc220f3c4a3a51f81cff30c923e6
6akjunyoung <6akjunyoung@gmail.com>
2024-07-04 16:47:27 +0900

    Restaurant Booking: 2. mocking TC 완성

---
+++ Project/Kata/TDD_1_Restaurant/Sample-Test1/MockedClass.cpp
@@ -0,0 +1,61 @@
+#include "../TDD_1_Restaurant/MailSender.cpp"
+#include "../TDD_1_Restaurant/SmsSender.cpp"
+#include "../TDD_1_Restaurant/BookingScheduler.cpp"
+
+#include <iostream>
+#include <stdexcept>
+
+using namespace std;
+
+class TestableBookingScheduler : public BookingScheduler
+{
+public:
+	TestableBookingScheduler(int capacityPerHour, tm dateTime)
+		: BookingScheduler{ capacityPerHour }, dateTime{ dateTime }
+	{}
+
+protected:
+	time_t getNow() override
+	{
+		return mktime(&dateTime);
+	}
+
+private:
+	tm dateTime;
+};
+
+class TestableMailSender : public MailSender
+{
+public:
+	void sendMail(Schedule* schedule)
+	{
+		cout << "Send E-mail (TEST)" << endl;
+		sendMethodIsCalled = true;
+	}
+
+	bool isSendMethodCalled()
+	{
+		return sendMethodIsCalled;
+	}
+
+private:
+	bool sendMethodIsCalled = false;
+};
+
+class TestableSmsSender : public SmsSender
+{
+public:
+	void send(Schedule* schedule) override
+	{
+		cout << "Send SMS (TEST)" << endl;
+		sendMethodIsCalled = true;
+	}
+
+	bool isSendMethodCalled()
+	{
+		return sendMethodIsCalled;
+	}
+
+private:
+	bool sendMethodIsCalled = false;
+};
--- Project/Kata/TDD_1_Restaurant/Sample-Test1/test.cpp
+++ Project/Kata/TDD_1_Restaurant/Sample-Test1/test.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "../TDD_1_Restaurant/BookingScheduler.cpp"
+#include "MockedClass.cpp"
+
 
 #include <iostream>
 #include <stdexcept>
@@ -12,6 +14,11 @@ protected:
 	{
 		NOT_ON_THE_HOUR = getTime(2021, 3, 26, 9, 05);
 		ON_THE_HOUR = getTime(2021, 3, 26, 9, 00);
+		SUNDAY_ON_THE_HOUR = getTime(2021, 3, 28, 9, 00);
+		MONDAY_ON_THE_HOUR = getTime(2021, 3, 29, 9, 00);
+
+		bookingScheduler.setSmsSender(&testableSmsSender);
+		bookingScheduler.setMailSender(&testableMailSender);
 	}
 
 public:
@@ -31,11 +38,16 @@ public:
 
 	tm NOT_ON_THE_HOUR;
 	tm ON_THE_HOUR;
+	tm SUNDAY_ON_THE_HOUR;
+	tm MONDAY_ON_THE_HOUR;
 	Customer CUSTOMER{ "Fake name", "010-1234-5678" };
+	Customer CUSTOMER_WITH_MAIL{ "Fake name", "010-1234-5678", "test@test.com" };
 	const int UNDER_CAPACITY = 1;
 	const int CAPACITY_PER_HOUR = 3;
 
 	BookingScheduler bookingScheduler{ CAPACITY_PER_HOUR };
+	TestableSmsSender testableSmsSender;
+	TestableMailSender testableMailSender;
 };
 
 TEST_F(BookingItem, t1) {//예약은_정시에만_가능하다_정시가_아닌경우_예약불가) {
@@ -77,10 +89,6 @@ TEST_F(BookingItem, t3) { //시간대별_인원제한이_있다_같은_시간대
 		//assert
 		EXPECT_EQ(string{ "Number of people is over restaurant capacity per hour" }, string{ e.what() });
 	}
-	catch (...)
-	{
-		FAIL();
-	}
 }
 
 TEST_F(BookingItem, t4) { //시간대별_인원제한이_있다_같은_시간대가_다르면_Capacity_차있어도_스케쥴_추가_성공) {
@@ -99,21 +107,62 @@ TEST_F(BookingItem, t4) { //시간대별_인원제한이_있다_같은_시간대
 }
 
 TEST_F(BookingItem, t5) { //예약완료시_SMS는_무조건_발송) {
+	//arrange
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
 
+	//act
+	bookingScheduler.addSchedule(schedule);
+
+	//assert
+	EXPECT_TRUE(testableSmsSender.isSendMethodCalled());
 }
 
 TEST_F(BookingItem, t6) { //이메일이_없는_경우에는_이메일_미발송) {
+	//arrange
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
 
+	//act
+	bookingScheduler.addSchedule(schedule);
+
+	//assert
+	EXPECT_FALSE(testableMailSender.isSendMethodCalled());
 }
 
 TEST_F(BookingItem, t7) { //이메일이_있는_경우에는_이메일_발송) {
+	//arrange
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER_WITH_MAIL };
 
+	//act
+	bookingScheduler.addSchedule(schedule);
+
+	//assert
+	EXPECT_TRUE(testableMailSender.isSendMethodCalled());
 }
 
 TEST_F(BookingItem, t8) { //현재날짜가_일요일인_경우_예약불가_예외처리) {
+	//arrange
+	BookingScheduler* sundayScheduler = new TestableBookingScheduler(CAPACITY_PER_HOUR, SUNDAY_ON_THE_HOUR);
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
 
+	try {
+		//act
+		sundayScheduler->addSchedule(schedule);
+		FAIL();
+	}
+	catch (runtime_error& e) {
+		//assert
+		EXPECT_EQ(string{ "Booking system is not available on sunday" }, string{ e.what() });
+	}
 }
 
 TEST_F(BookingItem, t9) { //현재날짜가_일요일이_아닌경우_예약가능) {
+	//arrange
+	BookingScheduler* sundayScheduler = new TestableBookingScheduler(CAPACITY_PER_HOUR, MONDAY_ON_THE_HOUR);
+	Schedule* schedule = new Schedule{ ON_THE_HOUR, UNDER_CAPACITY, CUSTOMER };
 
+	//act
+	sundayScheduler->addSchedule(schedule);
+
+	//assert
+	sundayScheduler->hasSchedule(schedule);
 }
